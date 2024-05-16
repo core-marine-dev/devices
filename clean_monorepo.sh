65d29d7b3c5419603d2bd0d5c0f22c34babaae6f
Cristóbal Contreras Rubio <crisconru@gmail.com>
2024-05-16 08:15:57 +0200

    remove nested node_modules folders too

--- clean_monorepo.sh
+++ clean_monorepo.sh
@@ -1,5 +1,5 @@
 rm -rf node_modules \
-  packages/nmea-parser/dist \
-  packages/norsub-emru/dist \
-  packages/septentrio-sbf/dist \
-  packages/thelmabiotel-tblive/dist
+  packages/nmea-parser/node_modules packages/nmea-parser/dist \
+  packages/norsub-emru/node_modules packages/norsub-emru/dist \
+  packages/septentrio-sbf/node_modules packages/septentrio-sbf/dist \
+  packages/thelmabiotel-tblive/node_modules packages/thelmabiotel-tblive/dist
