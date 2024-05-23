4176098a55ef266e3eacb728d69c033079c4b46f
Cristóbal Contreras Rubio <crisconru@gmail.com>
2024-05-23 18:49:50 +0200

    SBG-ECOM-NODERED: v0.0.1 with the minimum to be publish

---
+++ packages/sbg-ecom-nodered/vitest.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  test: {
+    // ... Specify options here.
+    coverage: {
+      exclude: [
+        '**/*/constants.ts',
+        '**/*/types.ts'
+      ]
+    }
+  }
+})
