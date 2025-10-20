c60be74de1ca522cc28d62d49a509c4333ff03a4
Cristobal Contreras Rubio <cru@core-marine.com>
2025-10-20 17:56:25 +0200

    NORSUB-EMRU: v2.1.0 updated dependencies

--- packages/norsub-emru/vitest.config.ts
+++ packages/norsub-emru/vitest.config.ts
@@ -2,14 +2,14 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
+    include: ['tests/**/*.test.ts'],
     // ... Specify options here.
     coverage: {
       exclude: [
         '**/*/constants.ts',
         '**/*/types.ts',
         'dist/*',
-        'yaml-to-json.js',
-        '*.config.ts'
+        'tsup.config.ts'
       ]
     }
   }
